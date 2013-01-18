8fb0e28a31132feb111b901678534a7385b89d2f
gregorybooma <booma@mit.edu>
2013-01-18 10:30:18 -0500

    changed to port 80 for lamp app

--- node-app/config.js
+++ node-app/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   // Path for LAMP application running the game, including port if appropriate
-  "lampPath": "http://localhost:8888",
+  "lampPath": "http://localhost:80",
 
   // Port for this Node.js application to run on
   "port": 8080
